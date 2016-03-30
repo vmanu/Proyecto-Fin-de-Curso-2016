c71efea6920d3f48808d75077229a31ab0f6bba2
vmanu <vmanuoveuh@hotmail.com>
2016-03-30 19:21:19 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	ProyectoEmpresaVictorManuelOviedo/Index.php

---
+++ DataPPTGame/src/main/java/com/mycompany/datapptgame/GameType.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.datapptgame;
+
+/**
+ *
+ * @author ivanp
+ */
+public enum GameType {
+    JUEGO3,JUEGO5,JUEGO9,ANY,NONE
+}
---
+++ DataPPTGame/src/main/java/com/mycompany/datapptgame/Player.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.datapptgame;
+
+/**
+ *
+ * @author ivanp
+ */
+public class Player {
+    private String namePlayer;
+    private GameType tipoJuego;
+    private RoundsNumber numberOfRounds;
+    private boolean playing;
+
+    public Player() {
+    }
+
+    public Player(String namePlayer, GameType tipoJuego, RoundsNumber numberOfRounds, boolean playing) {
+        this.namePlayer = namePlayer;
+        this.tipoJuego = tipoJuego;
+        this.numberOfRounds = numberOfRounds;
+        this.playing = playing;
+    }
+
+    public String getNamePlayer() {
+        return namePlayer;
+    }
+
+    public void setNamePlayer(String namePlayer) {
+        this.namePlayer = namePlayer;
+    }
+
+    public GameType getTipoJuego() {
+        return tipoJuego;
+    }
+
+    public void setTipoJuego(GameType tipoJuego) {
+        this.tipoJuego = tipoJuego;
+    }
+
+    public RoundsNumber getNumberOfRounds() {
+        return numberOfRounds;
+    }
+
+    public void setNumberOfRounds(RoundsNumber numberOfRounds) {
+        this.numberOfRounds = numberOfRounds;
+    }
+
+    public boolean isPlaying() {
+        return playing;
+    }
+
+    public void setPlaying(boolean playing) {
+        this.playing = playing;
+    }
+}
