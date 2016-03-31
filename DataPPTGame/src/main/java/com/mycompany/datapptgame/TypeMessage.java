cab3ba34dde7ba020146a33aafb29cf34509db63
vmanu <vmanuoveuh@hotmail.com>
2016-03-31 21:08:13 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	ProyectoEmpresaVictorManuelOviedo/Index.php

--- DataPPTGame/src/main/java/com/mycompany/datapptgame/TypeMessage.java
+++ DataPPTGame/src/main/java/com/mycompany/datapptgame/TypeMessage.java
@@ -10,5 +10,5 @@ package com.mycompany.datapptgame;
  * @author oscar
  */
 public enum TypeMessage {
-    CONEXION,PARTIDA,RESPUESTA,DESCONEXION
+    CONEXION,PARTIDA,RESPUESTA,DESCONEXION,NOMBRE
 }
