7122a9a2dc8c84dd7172a7a96df12eff4695984a
vmanu <vmanuoveuh@hotmail.com>
2016-04-09 20:49:39 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	ProyectoEmpresaVictorManuelOviedo/Index.php

--- DesktopApp/src/vista/FXMLController.java
+++ DesktopApp/src/vista/FXMLController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package desktopapp;
+package vista;
 
 import java.net.URL;
 import java.util.ResourceBundle;
