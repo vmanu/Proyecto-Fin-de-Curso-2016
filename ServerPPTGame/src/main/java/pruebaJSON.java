7a241e92b2f7a8751b3f3604af26d69cce016d98
ivanPellisso <ivanpellisso.red@gmail.com>
2016-04-15 16:23:31 +0200

    20:23

---
+++ ServerPPTGame/src/main/java/pruebaJSON.java
@@ -0,0 +1,38 @@
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mycompany.datapptgame.MetaMessage;
+import com.mycompany.datapptgame.TypeMessage;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author IvPR
+ */
+public class pruebaJSON {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            MetaMessage mm=new MetaMessage();
+            mm.setContent(3);
+            mm.setType(TypeMessage.NOMBRE);
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+            System.out.println(mapper.writeValueAsString(mm));
+        } catch (JsonProcessingException ex) {
+            Logger.getLogger(pruebaJSON.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
